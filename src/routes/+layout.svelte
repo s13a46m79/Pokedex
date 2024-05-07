34cc0a1b8e02f21a4184646179616853a598083e
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-05-07 19:48:07 +0800

    add layout page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="w-full bg-black text-white p-3 font-bold text-xl">
+  <a href="/">Pokedex</a>
+</div>
+
+<slot class="w-screen bg-gray-100"></slot>
